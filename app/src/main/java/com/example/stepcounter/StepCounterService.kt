101f5ce1700b1f367b39971ac9bbbb82d68d5dfd
Redish03 <pluto0303@pukyong.ac.kr>
2025-11-19 15:13:29 +0900

    refactor(StepCounterService): 걸음수가 바뀔때마다 UI 업데이트가 아닌 2초마다 한번 씩 업데이트 하도록 변경

--- app/src/main/java/com/example/stepcounter/StepCounterService.kt
+++ app/src/main/java/com/example/stepcounter/StepCounterService.kt
@@ -4,7 +4,6 @@ import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
-import android.app.Service
 import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
@@ -17,15 +16,21 @@ import android.os.IBinder
 import android.util.Log
 import android.widget.Toast
 import androidx.core.app.NotificationCompat
-import com.example.stepcounter.databinding.ActivityMainBinding
+import androidx.lifecycle.LifecycleService
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
-class StepCounterService : Service(), SensorEventListener {
+class StepCounterService : LifecycleService(), SensorEventListener {
 
     private lateinit var sensorManager: SensorManager
     private var stepCountSensor: Sensor? = null
-    private var currentSteps = 0
     private lateinit var walkPrefs: SharedPreferences
     private lateinit var notificationManager: NotificationManager
+
+    private var currentSteps = 0
+    private var lastSavedSteps = 0
 
     override fun onCreate() {
         super.onCreate()
@@ -35,36 +40,22 @@ class StepCounterService : Service(), SensorEventListener {
         notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
         initCurrentSteps()
-
-        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
-        stepCountSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_DETECTOR)
-
-        checkSensorAvailable()
+        setupSensor()
+        launchUpdateLoop()
     }
 
     private fun initCurrentSteps() {
         currentSteps = walkPrefs.getInt(StepCounterUtil.KEY_CURRENT_STEPS, 0)
         Log.d("StepCounterService", "초기 걸음수 불러옴: $currentSteps")
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        sensorManager.unregisterListener(this)
+        lastSavedSteps = currentSteps
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        super.onStartCommand(intent, flags, startId)
         Log.d("StepCounterService", "서비스 시작")
 
-        startForegroundService()
+        startForegroundServiceNotification()
         return START_STICKY
-    }
-
-    override fun onBind(p0: Intent?): IBinder? {
-        return null
-    }
-
-    override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
-        // TODO("Not yet implemented")
     }
 
     override fun onSensorChanged(event: SensorEvent?) {
@@ -72,21 +63,43 @@ class StepCounterService : Service(), SensorEventListener {
             if (event.sensor.type == Sensor.TYPE_STEP_DETECTOR) {
                 if (event.values[0] == 1.0f) {
                     currentSteps++
-                    Log.d("StepCounterService", "걸음 수 : $currentSteps")
-
-                    saveStepsToPrefs(currentSteps)
-
-                    Log.d("StepCounterService", ">>> MainActivity로 방송(Broadcast) 송신 시도...")
-                    sendSetUpdateBroadcast(currentSteps)
-
-                    val updatedNotification = createNotification()
-                    notificationManager.notify(NOTIFICATION_ID, updatedNotification)
                 }
             }
         }
     }
 
-    private fun checkSensorAvailable() {
+    private fun launchUpdateLoop() {
+        lifecycleScope.launch(Dispatchers.Default) {
+            while (true) {
+                delay(2000)
+
+                if (currentSteps != lastSavedSteps) {
+                    Log.d("StepCounterService", "변경 감지: $lastSavedSteps -> $currentSteps")
+
+                    saveStepsToPrefs(currentSteps)
+
+                    launch(Dispatchers.Main) {
+                        sendStepUpdateBroadcast(currentSteps)
+                    }
+
+                    updateNotification(currentSteps)
+                    lastSavedSteps = currentSteps
+                }
+            }
+        }
+    }
+
+    private fun sendStepUpdateBroadcast(steps: Int) {
+        val intent = Intent(StepCounterUtil.ACTION_STEP_UPDATED)
+        intent.putExtra(StepCounterUtil.KEY_CURRENT_STEPS, steps)
+        intent.setPackage(packageName)
+        sendBroadcast(intent)
+    }
+
+    private fun setupSensor() {
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        stepCountSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_DETECTOR)
+
         if (stepCountSensor == null) {
             Toast.makeText(this, "만보기 센서가 없습니다", Toast.LENGTH_SHORT)
         } else {
@@ -99,7 +112,7 @@ class StepCounterService : Service(), SensorEventListener {
     }
 
     // 포그라운드 서비스 알림 설정
-    private fun startForegroundService() {
+    private fun startForegroundServiceNotification() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             // Android 8.0(Oreo) 이상에는 알림 채널 필요
             val channel = NotificationChannel(
@@ -107,47 +120,70 @@ class StepCounterService : Service(), SensorEventListener {
                 "만보기",
                 NotificationManager.IMPORTANCE_LOW // 중요도를 낮춰 소리랑 진동을 없앰
             )
-
             notificationManager.createNotificationChannel(channel)
         }
 
-        // 알림 생성
-        val notification = createNotification()
-
+        // 알림 생성 -> 초기에는 현재 걸음수로 초기화
+        val notification = createNotification(currentSteps)
         // ForeGround 서비스 시작 (ID와 알림 전달)
         startForeground(NOTIFICATION_ID, notification)
     }
 
     private fun saveStepsToPrefs(steps: Int) {
-        walkPrefs.edit().putInt(StepCounterUtil.KEY_CURRENT_STEPS, steps).apply()
+        walkPrefs.edit().putInt(StepCounterUtil.KEY_CURRENT_STEPS, steps).commit()
     }
 
-    private fun sendSetUpdateBroadcast(steps: Int) {
-        val intent = Intent(StepCounterUtil.ACTION_STEP_UPDATED)
-        intent.putExtra(StepCounterUtil.KEY_CURRENT_STEPS, steps)
+    private fun createNotification(steps: Int): Notification {
+        val pendingIntent = Intent(this, MainActivity::class.java).let {
+            PendingIntent.getActivity(this, 0, it, PendingIntent.FLAG_IMMUTABLE)
+        }
 
-        intent.setPackage(packageName)
-        sendBroadcast(intent)
-    }
-
-    private fun createNotification(): Notification {
-//        val pendingIntent = Intent(this, MainActivity::class.java).let {
-//            PendingIntent.getActivity(this, 0, PendingIntent.FLAG_IMMUTABLE)
-//        }
-
-        val contextText = "현재 걸음수 : $currentSteps"
+        val contextText = "현재 걸음수 : $steps"
 
         return NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID)
             .setContentTitle("만보기앱")
             .setContentText(contextText)
-//            .setContentIntent(pendingIntent)
+            .setContentIntent(pendingIntent)
             .setSmallIcon(R.mipmap.ic_launcher) // TODO: 앱 아이콘으로 변경
             .setOnlyAlertOnce(true)
             .build()
     }
 
+    private fun updateNotification(steps: Int) {
+        val notification = createNotification(steps)
+        notificationManager.notify(NOTIFICATION_ID, notification)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        sensorManager.unregisterListener(this)
+    }
+
+    override fun onBind(p0: Intent): IBinder? {
+        super.onBind(p0)
+        return null
+    }
+
+    override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
+        // TODO("Not yet implemented")
+    }
+
     companion object {
         private const val NOTIFICATION_ID = 1
         private const val NOTIFICATION_CHANNEL_ID = "step_counter_channel"
+
+        fun startService(context: Context) {
+            val intent = Intent(context, StepCounterService::class.java)
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                context.startForegroundService(intent)
+            } else {
+                context.startService(intent)
+            }
+        }
+
+        fun stopService(context: Context) {
+            val intent = Intent(context, StepCounterService::class.java)
+            context.stopService(intent)
+        }
     }
 }
